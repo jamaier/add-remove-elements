c1f880424d077d67b21554760be835f1dec80334
Jacob Maier <jacobamaier@gmail.com>
2023-01-17 10:50:52 -0800

    Update heading and P, img and second heading element.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-window.onload = function() {
-  
-}
+window.addEventListener( "load", function() {
+
+  const headOne = document.querySelector("h1");
+  headOne.textContent = "Webpage Recreation Practice";
+
+  const pOne = document.createElement("p");
+  pOne.append("The HTML of this webpage was created with JavaScript.");
+  document.querySelector("body").append(pOne);
+
+  const image = document.createElement("img");
+  image.className = "src= "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001/900" alt="This is an image of a Multicolored Tanager bird from birdsoftheworld.org" style="width:50%"";
+  document.
+
+});
